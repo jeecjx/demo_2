4388a890dbb3fa50e1f37bd9f670cf3cfaccb501
jeecjx <1468102190@qq.com>
2019-10-08 17:35:50 +0800

    添加了一个~

--- src/main/java/com/demo/Test.java
+++ src/main/java/com/demo/Test.java
@@ -5,6 +5,7 @@ package com.demo;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!~");
+
     }
 }
